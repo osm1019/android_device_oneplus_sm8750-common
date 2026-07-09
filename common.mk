8d36ff40f2417b58e7b9c630d9afca57870e4833
Oscar <osm1019@gmail.com>
2026-07-08 18:53:45 -0700

    Revert "sm8750-common: Add oplus-sm8750 ALS [SQUASHED]"
    This reverts commit bedd00996e152682c257c9024c5eab0c843c2089.

--- common.mk
+++ common.mk
@@ -584,7 +584,6 @@ endif
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.oplus-multihal \
-    vendor.lineage.oplus_als.service \
     sensors.dynamic_sensor_hal \
     sensors.qsh_wrapper
 
